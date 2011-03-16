d1a314e12e3962eab24cca475c52c52488855252
fhsrobotics <fairviewrobotics@gmail.com>
2011-03-16 17:44:23 -0600

    Added some state stuff to use for the special buttons.

--- src/org/fhsrobotics/robot/control/JoystickControl.java
+++ src/org/fhsrobotics/robot/control/JoystickControl.java
@@ -10,10 +10,28 @@ public class JoystickControl extends Control
 {
 	//The joysticks used to control the drive.
 	Joystick rJoy, lJoy;
-	
+
+	//Robot can be disabled by the disable combination.
 	boolean enabled;
 	//If the toggle was pressed last update.
-	boolean enableEdge;
+	boolean enableLast;
+
+	//---Specraise button---
+	private static final int SPECRAISE_BUTTON = 4; //Button #
+	private static final float SPECRAISE_AMOUNT = 0.5f; //In seconds.
+	private int specraiseState;
+	private static final int SPECSTATE_WAIT = 0;
+	private static final int SPECSTATE_DROP = 1;
+	private static final int SPECSTATE_RAISE = 2;
+
+	//---Rollback button---
+	private static final int ROLLBACK_BUTTON = 5; //Button #
+	private static final float ROLLBACK_TIME = 0.5f; //How long the robot will RETREAT!!!!
+	private int rollbackState;
+	private static final int ROLLBACK_WAIT = 0;
+	private static final int ROLLBACK_DROP = 1;
+	private static final int ROLLBACK_BACKUP = 2;
+
 	
 	public JoystickControl()
 	{
@@ -23,39 +41,60 @@ public class JoystickControl extends Control
 		lJoy = new Joystick(2);
 
 		enabled = true;
-		enableEdge = false;
+		enableLast = false;
 	}
 
 	public void update()
 	{
+		double transX = -rJoy.getX();
+		double transY = -rJoy.getY()-lJoy.getY();
+		double rotate = -lJoy.getX();
+
+		//---Specraise code---
+		switch(specraiseState)
+		{
+			case SPECSTATE_WAIT:
+			break;
+			case SPECSTATE_DROP:
+			break;
+			
+		}
+
+		//---Drive, if the disable combination not pressed---
 		if(enabled)
-			drive.move(-rJoy.getX(), -rJoy.getY()-lJoy.getY(), -lJoy.getX());
+			drive.move(transX, transY, rotate);
 		else
 			drive.setWheels(0.0, 0.0, 0.0, 0.0);
 
+		//---Disable combination---
 		boolean disablePress = (rJoy.getRawButton(6) && rJoy.getRawButton(7) && rJoy.getRawButton(11) && rJoy.getRawButton(10))
 			|| (rJoy.getRawButton(6) && rJoy.getRawButton(7) && rJoy.getRawButton(11) && rJoy.getRawButton(10));
-		if(disablePress && !enableEdge)
+		if(disablePress && !enableLast)
 		{
 			enabled = !enabled;
 		}
-		enableEdge = disablePress;
+		enableLast = disablePress;
 
-		double forkspeed = (rJoy.getRawButton(3)||lJoy.getRawButton(3)?1:0)
-			             - (rJoy.getRawButton(2)||lJoy.getRawButton(2)?1:0);
-		forkspeed *= (rJoy.getRawButton(1) || lJoy.getRawButton(1)) ? 1.0 : 0.4;
-		if(sense.forkBottom.get())
+		//---Forklift control---
+		if(rollbackState == ROLLBACK_WAIT && specraiseState == SPECSTATE_WAIT)
 		{
-			forkspeed = Math.max(forkspeed, 0);
-		}
-		if(sense.forkTop.get())
-		{
-			forkspeed = Math.min(forkspeed, 0);
+			double forkspeed = (rJoy.getRawButton(3)||lJoy.getRawButton(3)?1:0)
+							 - (rJoy.getRawButton(2)||lJoy.getRawButton(2)?1:0);
+			forkspeed *= (rJoy.getRawButton(1) || lJoy.getRawButton(1)) ? 1.0 : 0.4;
+			if(sense.forkBottom.get())
+			{
+				forkspeed = Math.max(forkspeed, 0);
+			}
+			if(sense.forkTop.get())
+			{
+				forkspeed = Math.min(forkspeed, 0);
+			}
+
+			drive.setForklift(
+				Math.max(Math.min(forkspeed, 1),-1)
+			);
 		}
 
-		drive.setForklift(
-			Math.max(Math.min(forkspeed, 1),-1)
-		);
 //		System.out.println("Right: "+rJoy.getX()+","+rJoy.getY()+" Left: "+lJoy.getX()+","+lJoy.getY());
 //		System.out.println("13: "+sense.forkBottom.get());
 	}
